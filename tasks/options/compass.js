bd74a14570be774445a4f11e774c10c92ae0c7b1
Erik Akers <erikakers@gmail.com>
2014-03-28 17:16:33 -0500

    adding support for multiple import paths

--- tasks/options/compass.js
+++ tasks/options/compass.js
@@ -1,9 +1,17 @@
+/*
+	Compiling for SCSS compass.
+	CSS Min is handled in another task this is just for SCSS compiling.
+	importPath should be changed if the project is using any third party SCSS libraries.
+ */
 module.exports = {
 	compass: {
 		options: {
 			sassDir: '<%= config.src %>/scss',
 			cssDir: '<%= config.app %>/css',
-			importPath: '<%= config.vendor %>/foundation/scss',
+			importPath: [
+				'<%= config.vendor %>/foundation/scss',
+				'<%= config.vendor %>/bourbon/app/assets/stylesheets'
+			]
 		}
 	}
 };
