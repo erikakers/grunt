1b6ae7cfd0511868d2ade770d044c42601ed9daa
Erik Akers <erikakers@gmail.com>
2014-03-28 17:03:16 -0500

    adding compass task

---
+++ tasks/options/compass.js
@@ -0,0 +1,9 @@
+module.exports = {
+	compass: {
+		options: {
+			sassDir: '<%= config.src %>/scss',
+			cssDir: '<%= config.app %>/css',
+			importPath: '<%= config.vendor %>/foundation/scss',
+		}
+	}
+};
