82829b91e128a0700a5ff22463a6d4337f7367e1
Erik Akers <erikakers@gmail.com>
2014-04-09 17:29:59 -0500

    using grunt contrib sass

---
+++ grunt/tasks/sass.js
@@ -0,0 +1,17 @@
+module.exports = {
+	dev: {
+		options: {
+			sourcemap: true,
+			loadPath: [
+				'<%= config.vendor %>/foundation/scss'
+			]
+		},
+		files: [{
+			expand: true,
+			cwd: '<%= config.src %>/scss/',
+			src: ['*.scss'],
+			dest: '<%= config.app %>/css/',
+			ext: '.css'
+		}]
+	}
+};
