4f7e3891fa3cab822900a5cd1361e1263702630c
Erik Akers <erikakers@gmail.com>
2014-04-09 17:41:55 -0500

    updating concat file and options

--- grunt/tasks/concat.js
+++ grunt/tasks/concat.js
@@ -1,43 +1,17 @@
 module.exports = {
-	vendor: {
-		files: [
-			// List any additional files that will be concatted into a single file
-			// Follow the same object pattern if a secondary concatted file is needed
-			// ie. touch event scripts
-			{
-				src: [
-					'<%= base.src %>/components/jquery/jquery.js',
-					'<%= base.src %>/components/underscore/underscore.js'
-				],
-				dest: '<%= base.build %>/scripts/plugins.js'
-			},
-			{
-				src: [
-					'<%= base.src %>/components/es5-shim/es5-shim.js'
-				],
-				dest: '<%= base.build %>/scripts/polyfills.js'
-			}
-		]
+	options: {
+		banner: '<%= banner %>'
 	},
-	head: {
-		files: [
-			{
-				src: [
-					'<%= base.src %>/components/modernizr/modernizr.js'
-				],
-				dest: '<%= base.build %>/scripts/head.js'
-			}
-		]
+	vendor: {
+		src: [
+			'<%= config.vendor %>/jquery/dist/jquery.js'
+		],
+		dest: '<%= config.app %>/js/plugins.js'
 	},
 	dev: {
-		files: [
-			{
-				src: [
-					'<%= base.src %>/js/namespacing.js',
-					'<%= base.src %>/js/config.js'
-				],
-				dest: '<%= base.build %>/scripts/main.js'
-			}
-		]
+		src: [
+			'<%= config.src %>/javascript/classes/{,*/}*.js'
+		],
+		dest: '<%= config.app %>/js/main.js'
 	}
 }
