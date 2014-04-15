2254fd3394620872e6ba98fb6c519410381837d8
Erik Akers <erikakers@gmail.com>
2014-04-15 16:36:21 -0500

    adding react task

---
+++ grunt/tasks/react.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+	compile: {
+		files: [
+			{
+				expand: true,
+				cwd: '<%= base.src %>/javascript/jsx',
+				src: ['**/*.jsx'],
+				dest: '<%= base.src %>/javascript/views',
+				ext: '.js'
+			}
+		]
+	}
+};
