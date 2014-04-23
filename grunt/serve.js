395a35a04145a45c6943f8904b77d333d9886711
Erik Akers <erikakers@gmail.com>
2014-04-23 17:18:25 -0500

    removing target function

--- grunt/serve.js
+++ grunt/serve.js
@@ -3,9 +3,6 @@ module.exports = function(grunt) {
 	'use strict';
 
 	grunt.registerTask('serve', function(target) {
-		if (target === 'dist') {
-			return grunt.task.run(['build', 'connect:dist:keepalive']);
-		}
 
 		grunt.task.run([
 			'clean:server',
