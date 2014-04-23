737de5b183caa6340b9e6b5f90514535ceb85899
Erik Akers <erikakers@gmail.com>
2014-04-23 15:46:13 -0500

    file clean up

--- grunt/tasks/connect.js
+++ grunt/tasks/connect.js
@@ -1,19 +1,19 @@
 // connect task options
 module.exports = {
-            options: {
-                port: 9000,
-                open: true,
-                livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    middleware: function(connect) {
-                        return [
-                            connect.static('app')
-                        ];
-                    }
-                }
-            }
+	options: {
+		port: 9000,
+		open: true,
+		livereload: 35729,
+		// Change this to '0.0.0.0' to access the server from outside
+		hostname: 'localhost'
+	},
+	livereload: {
+		options: {
+			middleware: function(connect) {
+				return [
+					connect.static('app')
+				];
+			}
+		}
+	}
 };
