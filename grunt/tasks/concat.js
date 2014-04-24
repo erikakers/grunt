d9f8b9d8385a71a4e649939893cfe87d6dee0fbc
Erik Akers <erikakers@gmail.com>
2014-04-24 15:32:50 -0500

    adding sample folder structure

--- grunt/tasks/concat.js
+++ grunt/tasks/concat.js
@@ -2,15 +2,26 @@ module.exports = {
 	options: {
 		banner: '<%= banner %>'
 	},
+	head: {
+		src: [
+			// Any Shims
+		],
+		dest: '<%= config.app %>/scripts/vendor/head.js'
+	},
 	vendor: {
 		src: [
 			'<%= config.vendor %>/jquery/dist/jquery.js'
 		],
-		dest: '<%= config.app %>/scripts/plugins.js'
+		dest: '<%= config.app %>/scripts/vendor/plugins.js'
 	},
 	dev: {
 		src: [
-			'<%= config.src %>/javascript/classes/{,*/}*.js'
+			'<%= config.src %>/javascript/classes/{,*/}*.js',
+			'<%= config.src %>/javascript/helpers/{,*/}*.js',
+			'<%= config.src %>/javascript/features/{,*/}*.js',
+			'<%= config.src %>/javascript/models/{,*/}*.js',
+			'<%= config.src %>/javascript/controllers/{,*/}*.js',
+			'<%= config.src %>/javascript/views/{,*/}*.js'
 		],
 		dest: '<%= config.app %>/scripts/main.js'
 	}
