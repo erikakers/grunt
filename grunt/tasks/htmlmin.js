ecee941442dd2fbe525f79386daa4ca2f700a497
Erik Akers <erikakers@gmail.com>
2014-04-24 15:34:10 -0500

    using the processhtml file for minify

--- grunt/tasks/htmlmin.js
+++ grunt/tasks/htmlmin.js
@@ -1,8 +1,12 @@
 module.exports = {
 	dist: {
+		options: {
+			removeComments: true,
+			collapseWhitespace: true
+		},
 		files: [{
 			expand: true,
-			cwd: '<%= config.app %>',
+			cwd: '<%= config.temp %>',
 			src: '*.html',
 			dest: '<%= config.dist %>'
 		}]
