ee12aa9757f0f9f50e6e5b4876ead2e982a132da
Erik Akers <erikakers@gmail.com>
2014-04-28 14:57:01 -0500

    adding assemble task

---
+++ grunt/options/assemble.js
@@ -0,0 +1,12 @@
+module.exports = {
+	options: {
+		layoutdir: '<%= config.src %>/html/layouts',
+		layout: 'default.hbs',
+		partials: ['<%= config.src %>/html/partials/{,*/}*.html'],
+		data: ['<%= config.src %>/html/data/*.{json,yml}']
+	},
+	pages: {
+		src: ['<%= config.src %>/html/pages/{,*/}*.hbs'],
+		dest: '<%= config.app %>/'
+	}
+};
