d0902af46bdfbcdf7aae82b0de558abeaf4469db
Erik Akers <erikakers@gmail.com>
2014-04-28 17:56:39 -0500

    adding flatten true

--- grunt/options/assemble.js
+++ grunt/options/assemble.js
@@ -3,7 +3,8 @@ module.exports = {
 		layoutdir: '<%= config.src %>/html/layouts',
 		layout: 'default.hbs',
 		partials: ['<%= config.src %>/html/partials/{,*/}*.hbs'],
-		data: ['<%= config.src %>/html/data/*.{json,yml}']
+		data: ['<%= config.src %>/html/data/*.{json,yml}'],
+		flatten: true
 	},
 	pages: {
 		src: ['<%= config.src %>/html/pages/{,*/}*.hbs'],
