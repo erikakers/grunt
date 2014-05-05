4549b1549ebbf6b6bb25af7a4eb6b593854507bf
Erik Akers <erikakers@gmail.com>
2014-05-05 10:15:37 -0500

    updating modernizr task for 0.7.0

--- grunt/options/modernizr.js
+++ grunt/options/modernizr.js
@@ -1,16 +1,18 @@
 module.exports = {
-	devFile: '<%= config.vendor %>/modernizr/modernizr.js',
-	outputFile: '<%= config.app %>/scripts/vendor/modernizr.js',
-	extra: {
-		'shiv': true,
-		'load': true,
-		'mq': true,
-		'cssclasses': true
-	},
-	files: [
-		'<%= config.app %>/scripts/{,*/}*.js',
-		'<%= config.app %>/styles/{,*/}*.css',
-		'!<%= config.app %>/scripts/vendor/*'
-	],
-	uglify: false
+	dev:{
+		devFile: '<%= config.vendor %>/modernizr/modernizr.js',
+		outputFile: '<%= config.app %>/scripts/vendor/modernizr.js',
+		extra: {
+			'shiv': true,
+			'load': true,
+			'mq': true,
+			'cssclasses': true
+		},
+		customTests: [
+			'<%= config.app %>/scripts/{,*/}*.js',
+			'<%= config.app %>/styles/{,*/}*.css',
+			'!<%= config.app %>/scripts/vendor/*'
+		],
+		uglify: false
+	}
 };
