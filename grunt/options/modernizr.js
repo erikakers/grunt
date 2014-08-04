8f359d0e6b6a13134d4bc015bfca8e8a0161ba75
Erik Akers <erikakers@gmail.com>
2014-08-04 17:54:13 -0500

    updating Modernizr task

--- grunt/options/modernizr.js
+++ grunt/options/modernizr.js
@@ -1,21 +1,46 @@
 module.exports = {
-	dev:{
-		devFile: '<%= config.vendor %>/modernizr/modernizr.js',
-		outputFile: '<%= config.app %>/scripts/vendor/modernizr.js',
-		extra: {
-			'shiv': true,
-			'load': true,
-			'mq': true,
-			'cssclasses': true
-		},
-		parseFiles: true,
-		files: {
-			src: [
-				'<%= config.app %>/scripts/{,*/}*.js',
-				'<%= config.app %>/styles/{,*/}*.css',
-				'!<%= config.app %>/scripts/vendor/*'
-			]
-		},
-		uglify: false
-	}
+	dist: {
+    "devFile" : "<%= config.vendor %>/modernizr/modernizr.js",
+    "outputFile" : "<%= config.app %>/scripts/vendor/modernizr.js",
+
+    // Based on default settings on http://modernizr.com/download/
+    "extra" : {
+    	"shiv" : true,
+      "printshiv" : false,
+      "load" : true,
+      "mq" : true,
+      "cssclasses" : true
+  	},
+
+    // Based on default settings on http://modernizr.com/download/
+    "extensibility" : {
+    	"addtest" : false,
+      "prefixed" : false,
+      "teststyles" : false,
+      "testprops" : false,
+      "testallprops" : false,
+      "hasevents" : false,
+      "prefixes" : false,
+      "domprefixes" : false
+    },
+
+    "uglify" : false,
+    // Define any tests you want to implicitly include.
+    "tests" : [],
+    "parseFiles" : true,
+    "files" : {
+    	"src": [
+					'<%= config.src %>/javascript/{,*/}*.js',
+					'<%= config.src %>/scss/{,*/}*.scss',
+					'!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+    // When parseFiles = true, matchCommunityTests = true will attempt to
+    // match user-contributed tests.
+    "matchCommunityTests" : false,
+		// Add custom tests to this folder
+    "customTests" : [
+			"<%= config.src %>/javascript/moderinzr/*.js"
+		]
+  }
 };
